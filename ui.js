14be652e9aacebdfaae4a15e1625392abd037777
Mitchell Jolivette <jolivetteme@gmail.com>
2018-07-01 16:18:59 -0500

    Adding live maps

--- ui.js
+++ ui.js
@@ -1,10 +1,16 @@
 (function(){
-    $('.callouts').hide();
-    $('div.menu').show();
+    // $('.callouts').hide();
+    $('.callout p').hide();
+    $('div.menu').hide();
     $('.toggle').on('click',function(){
       $('.callouts').slideToggle();
     });
     $('.menu-toggle').on('click',function(){
         $('div.menu').slideToggle();
+    });
+    $('.callout-title').on('click',function(){
+      // $(this+' p').slideToggle();
+      $(this).siblings("p.callout-content").slideToggle();
+
     })
 })();
